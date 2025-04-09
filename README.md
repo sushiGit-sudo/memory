14f75e168fb23fc29a5a61f53346d642513e8d10
Bitwise Studio <benjaminstroup5507@gmail.com>
2025-04-08 22:17:07 -0400

    Edited Files

--- README.md
+++ README.md
@@ -3,11 +3,16 @@
 ### 'memory.h' is a Header File you can include in your `C/C++` projects to help manipulate memory. This module works in a fast and efficient way.
 ---
 ## `Class -> MemoryBlock`
-#### `MemoryBlock` is a template class that allows you to store any type of data with any size; this is mainly used for storing large groups of data.
-##### `T* m_Memory;` used to store the data in the block.
-##### `unsigned int m_MemorySize;` used to specify how much data
-##### `void clear();` is used to set the entire **MemoryBlock** to NULL; it does not call `delete[] m_Block;`
-##### `T& operator[](unsigned int index) const;` Allows you to access the data using `memory[0]`.
+#### The class `MemoryBlock` is a template class that allows you to store any type of data with any size; this is mainly used for storing large groups of data.
+##### `T* m_Memory;`: Used to store the data in the block.
+##### `unsigned int m_MemorySize;`: Used to specify how much data
+##### `void clear();`: Used to set the entire **MemoryBlock** to NULL; it does not call `delete[] m_Block;`
+##### `void CopyMemory(T* src, unsigned int size);`: Used to copy the src to the memory block.
+##### `void CopyMemory(MemoryBlock& memory);`: Used to copy another memory block to this memory block.
+##### `T& GetIndexedMemoryAddress(unsigned int index) const;`: Used to get the memory address of specific index in the memory block.
+##### `void SetIndexedMemory(T data, unsigned int index);`: Used to set an individual index on the memory block.
+##### `T& operator[](unsigned int index) const;`: Used to access the data using `memory[0]`.
+##### `void operator=(const char* string);`: Used to 
 ---
 > Us here at (c)Bitwise Studio are working to make this a better module.
 ---
--- src/memory.h
+++ src/memory.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define NULL 0
+
 template<typename T>
 class MemoryBlock
 {
@@ -7,13 +9,32 @@ private:
 	T* m_Memory;
 	unsigned int m_MemorySize;
 public:
+	MemoryBlock(const char* string);
 	MemoryBlock(unsigned int size);
 	~MemoryBlock();
 
 	void clear();
 
+	void CopyMemory(T* src, unsigned int size);
+	void CopyMemory(MemoryBlock& memory);
+
+	T& GetIndexedMemoryAddress(unsigned int index) const;
+	void SetIndexedMemory(T data, unsigned int index);
+
 	T& operator[](unsigned int index) const;
+	void operator=(const char* string);
+
+private:
+	unsigned int countCharPtr(const char* string);
 };
+
+template<typename T>
+inline MemoryBlock<T>::MemoryBlock(const char* string)
+	: m_Memory(nullptr), m_MemorySize(countCharPtr(string))
+{
+	m_Memory = new T[m_MemorySize];
+	CopyMemory((char*)string, m_MemorySize);
+}
 
 template<typename T>
 inline MemoryBlock<T>::MemoryBlock(unsigned int size)
@@ -39,7 +60,56 @@ inline void MemoryBlock<T>::clear()
 }
 
 template<typename T>
+inline void MemoryBlock<T>::CopyMemory(T* src, unsigned int size)
+{
+	for (unsigned int i = 0; i < size; i++)
+	{
+		m_Memory[i] = src[i];
+	}
+}
+
+template<typename T>
+inline void MemoryBlock<T>::CopyMemory(MemoryBlock& memory)
+{
+	CopyMemory(memory.m_Memory, memory.m_MemorySize);
+}
+
+template<typename T>
+inline T& MemoryBlock<T>::GetIndexedMemoryAddress(unsigned int index) const
+{
+	return m_Memory[index];
+}
+
+template<typename T>
+inline void MemoryBlock<T>::SetIndexedMemory(T data, unsigned int index)
+{
+	m_Memory[index] = data;
+}
+
+template<typename T>
 inline T& MemoryBlock<T>::operator[](unsigned int index) const
 {
 	return m_Memory[index];
+}
+
+template<typename T>
+inline void MemoryBlock<T>::operator=(const char* string)
+{
+	if (m_Memory)
+		delete[] m_Memory;
+
+	m_MemorySize = countCharPtr(string);
+	m_Memory = new T[m_MemorySize];
+
+	CopyMemory((char*)string, m_MemorySize);
+}
+
+template<typename T>
+inline unsigned int MemoryBlock<T>::countCharPtr(const char* string)
+{
+	unsigned int count = 0;
+	for (unsigned int i = 0; string[i]; i++)
+		count++;
+
+	return count;
 }
